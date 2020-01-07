cb54b3a8de5e2b874004e35a7aa3ecae9e7b223a
David Wu <wudawei01@gmail.com>
2020-01-07 16:27:47 +0000

    Fix some bugs

--- app.rb
+++ app.rb
@@ -40,7 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess letter
+    begin
+      valid = @game.guess letter
+      flash[:message] = 'You have already used that letter' unless valid
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess'
+    end
     redirect '/show'
   end
   
@@ -51,6 +56,7 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    redirect '/new' if @game.word.empty?
     if @game.check_win_or_lose == :win
       redirect '/win'
     elsif @game.check_win_or_lose == :lose
@@ -61,11 +67,15 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/new' if @game.word.empty?
+    redirect '/show' unless @game.check_win_or_lose == :win
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/new' if @game.word.empty?
+    redirect '/show' unless @game.check_win_or_lose == :lose
     erb :lose # You may change/remove this line
   end
   
