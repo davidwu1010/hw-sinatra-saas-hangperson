8c1c9c029181440c2a40890dca838d214b58e2be
David Wu <wudawei01@gmail.com>
2020-01-07 12:06:12 +0000

    Implement word_with_guesses

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -42,6 +42,14 @@ class HangpersonGame
     end
   end
 
+  def word_with_guesses
+    result = ''
+    @word.chars do |letter|
+      result << ((@guesses.include? letter) ? letter : '-')
+    end
+    result
+  end
+
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
